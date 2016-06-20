ba6b3f0323e5b3d1a8bc975c49098185759d41ef
Akuma Huang <ihuangwj@gmail.com>
2016-06-20 21:57:26 +0800

    test: add db.js test

--- test/db.test.js
+++ test/db.test.js
@@ -1,5 +1,17 @@
 import test from 'ava'
+import Database from 'warehouse'
+import Model from 'warehouse/lib/model'
+import db from '../src/db'
 
-test('foo', t => {
-  t.pass()
+test('should get db', async t => {
+  const dbs = await db.get()
+  t.true(dbs instanceof Database)
+})
+
+test('should get model', async t => {
+  const model = await db.model('foo', {
+    name: String,
+    created: { type: Date, default: Date.now },
+  })
+  t.true(model instanceof Model)
 })
