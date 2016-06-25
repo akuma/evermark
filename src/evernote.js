f599d07a774d6cb200736839c3de39eef4376539
Akuma Huang <ihuangwj@gmail.com>
2016-06-25 22:33:17 +0800

    refactor: define evernote api error codes

--- src/evernote.js
+++ src/evernote.js
@@ -3,6 +3,28 @@ import { Evernote } from 'evernote'
 
 const debug = require('debug')('evernote')
 
+// Define Evernote API error codes
+const UNKNOWN = 1
+const BAD_DATA_FORMAT = 2
+const PERMISSION_DENIED = 3
+const INTERNAL_ERROR = 4
+const DATA_REQUIRED = 5
+const LIMIT_REACHED = 6
+const QUOTA_REACHED = 7
+const INVALID_AUTH = 8
+const AUTH_EXPIRED = 9
+const DATA_CONFLICT = 10
+const ENML_VALIDATION = 11
+const SHARD_UNAVAILABLE = 12
+const LEN_TOO_SHORT = 13
+const LEN_TOO_LONG = 14
+const TOO_FEW = 15
+const TOO_MANY = 16
+const UNSUPPORTED_OPERATION = 17
+const TAKEN_DOWN = 18
+const RATE_LIMIT_REACHED = 19
+const OBJECT_NOT_FOUND = 100
+
 export default class EvernoteClient {
   constructor({ token, china = true, sandbox = false } = { china: true, sandbox: false }) {
     if (!token) {
